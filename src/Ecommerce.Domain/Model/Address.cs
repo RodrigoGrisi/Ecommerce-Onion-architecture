a2a9460d8f638d6052e394c7997aa18f6c6e436d
Rodrigo Grisi <Rodricosta1995@gmail.com>
2024-11-15 18:28:08 -0300

    Adicionar arquivos de projeto.

---
+++ src/Ecommerce.Domain/Model/Address.cs
@@ -0,0 +1,13 @@
+﻿namespace Ecommerce.Domain.Model;
+
+public class Address
+{
+    public string Street { get; set; }
+    public int AddressNumber { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+    public string State { get; set; }
+    public string PhoneNumber { get; set; }
+    public string PostalCode { get; set; }
+
+}
---
+++ src/Ecommerce.Domain/Model/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace Ecommerce.Domain.Model;
+
+public class Customer
+{
+    public string Name { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty ;
+    public string Email { get; set; } = string.Empty;
+    public DateTime BirthDate { get; set; }
+    
+    public  Address Address { get; set; }
+
+    public string Cpf {  get; set; }
+    public bool isActive { get; set; }
+
+}
